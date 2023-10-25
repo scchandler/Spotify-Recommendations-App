c75af1dacbacab3ad3c1f7ee36a9369913ba6bc0
isoternet <maplesyrupquack@gmail.com>
2023-10-25 13:43:26 -0400

    Updated homepage, displays basic user info in a more pleasing way

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -5,15 +5,15 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   final Map<String, dynamic> userData; // Replace with your user data
 
-  HomePage({Key? key, required this.userData}) : super(key: key);
+  const HomePage({Key? key, required this.userData}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    JsonEncoder encoder = JsonEncoder.withIndent('  '); // Create a JSON encoder with indentation
+    JsonEncoder encoder = const JsonEncoder.withIndent('  '); // Create a JSON encoder with indentation
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Spotify Recommendations'),
+        title: const Text('Spotify Recommendations'),
       ),
       body: Container(
         decoration: BoxDecoration(
@@ -23,17 +23,33 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
+              const Text(
                 'User Profile Data',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              SizedBox(height: 20),
-              Text(
-                encoder.convert(userData), // Display pretty-printed JSON data
-                style: TextStyle(fontSize: 16),
-              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50.0),
+                    child: Image.network('${userData['images'][0]['url']}'),
+                  ),
+                  const SizedBox(width: 20),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '${userData['display_name']}', // Display pretty-printed JSON data
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      Text('Followers: ${userData['followers']['total']}'),
+                    ],
+                  ),
+                ],
+              )
             ],
-          ),
+          )
         ),
       ),
     );
